66ee9d0492dc32330657127cd9ab804f6c14a77e
TakamichiNomura <kennn3065@gmail.com>
2019-10-04 22:17:21 +0900

    p11하고 있지만 고쳐야 할 것이 2개정도 있다. 하나는 case1때 같은 이름이 나왔을 때 번호를 그대로 유지하는 것이고 하나는 case2때 get()함수가 잘 돌리지 않아.... 머리 터질것 같다~~~~

---
+++ p11/Main.java
@@ -0,0 +1,66 @@
+package p11;
+import java.util.*;
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Main
+{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        Nation nt;
+        Vector<Nation> v = new Vector<Nation>();
+        System.out.println(" *** 수도 맞추기 게임을 시작합니다.*** ");
+        while(true){
+            System.out.print("입력:1, 퀴즈:2, 종료:3>>");
+            int num_1 = s.nextInt();
+            int i = 0,j = 0;
+            ArrayList<String> array_name = new ArrayList<String>();
+            var array_capital = new ArrayList<String>();
+            switch(num_1){
+                case 1:
+                System.out.println("현재 "+ v.size() +"개 나라와 수도가 입력되어 있습니다.");
+                while(true){
+                    System.out.print("나라와 수도 입력"+ v.size() +">>");
+                    String name = s.next();
+                    if(name.equals("그만")){
+                        break;
+                    }
+                    array_name.add(j,name);
+                    String capital = s.next();
+                    array_capital.add(j,capital);
+                    nt = new Nation(name,capital);
+                    for(i = 0;i<v.size();i++){
+                        if(name.equals(array_name.get(i))){
+                            System.out.println(name +"는 이미 있습니다!");
+                        }
+                    }
+                    v.add(j,nt);
+                    j++;
+                }
+                break;
+                case 2:
+                while(true){
+                    int rand = (int)(Math.random()*array_name.size()+0);
+                    System.out.print(array_name.get(rand) +"의 수도는?");
+                    String name = s.next();
+                    if(name.equals("그만")){
+                        break;
+                    }
+                    if((array_capital.get(rand)).equals(name)){
+                        System.out.println("정답!!");
+                    }else{
+                        System.out.println("아닙니다!!");
+                    }
+                }
+                break;
+                case 3:
+                System.out.println("시스템을 종료하겠습니다.");
+                System.exit(0);
+            }
+        }
+    }
+}
---
+++ p11/Nation.java
@@ -0,0 +1,17 @@
+package p11;
+
+
+/**
+ * Write a description of class Nation here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Nation
+{
+    String country,capital;
+    public Nation(String country, String capital){
+        this.country = country;
+        this.capital = capital;
+    }
+}
